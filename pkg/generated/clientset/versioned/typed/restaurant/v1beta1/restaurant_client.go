7d1777da6db47da85ec822e17c631a4a0aeac88d
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2019-04-14 10:36:04 +0200

    Regen

--- pkg/generated/clientset/versioned/typed/restaurant/v1beta1/restaurant_client.go
+++ pkg/generated/clientset/versioned/typed/restaurant/v1beta1/restaurant_client.go
@@ -21,6 +21,7 @@ package v1beta1
 import (
 	v1beta1 "github.com/programming-kubernetes/custom-apiserver/pkg/apis/restaurant/v1beta1"
 	"github.com/programming-kubernetes/custom-apiserver/pkg/generated/clientset/versioned/scheme"
+	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	rest "k8s.io/client-go/rest"
 )
 
@@ -70,7 +71,7 @@ func setConfigDefaults(config *rest.Config) error {
 	gv := v1beta1.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
-	config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
+	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: scheme.Codecs}
 
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
